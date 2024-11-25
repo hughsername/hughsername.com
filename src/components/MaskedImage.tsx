5f5124ecf469dca4ca518b17cf841e29d37d508c
Hugh Dillon <hugh.dillon@udig.com>
2024-11-24 20:36:07 -0600

    tmp: save

--- src/components/MaskedImage.tsx
+++ src/components/MaskedImage.tsx
@@ -1,4 +1,5 @@
-import { Component } from 'solid-js';
+import { Component, createEffect, onMount } from 'solid-js';
+import { createSignal } from 'solid-js';
 import styles from './MaskedImage.module.css';
 
 interface Props {
@@ -10,17 +11,38 @@ interface Props {
 }
 
 export const MaskedImage: Component<Props> = (props) => {
-  const position = () => `${props.positionX ?? 0}% ${props.positionY ?? -450}px`;
+  let containerRef: HTMLDivElement | undefined;
+  const [yPosition, setYPosition] = createSignal('-450px');
+  
+  const updatePosition = () => {
+    if (!containerRef) return;
+    const containerWidth = containerRef.offsetWidth;
+    const scale = containerWidth / 677;
+    const basePosition = -450;
+    setYPosition(`${basePosition * scale}px`);
+  };
+
+  onMount(() => {
+    const observer = new ResizeObserver(updatePosition);
+    if (containerRef) {
+      observer.observe(containerRef);
+      updatePosition();
+    }
+    return () => observer.disconnect();
+  });
 
   return (
-    <div class={`${styles.container} masked-image ${props.class ?? ''}`}>
+    <div 
+      class={`${styles.container} masked-image ${props.class ?? ''}`}
+      ref={(el) => { containerRef = el; }}
+    >
       <div class={styles.imageContainer}>
         <div class={styles.gradient} />
         <img
           src={props.src}
           alt={props.alt}
           class={styles.image}
-          style={`object-position: ${position()};`}
+          style={`object-position: ${props.positionX ?? 0}% ${yPosition()};`}
         />
       </div>
     </div>
